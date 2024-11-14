d64b5dfb7ca66a4194fafed336a9c67c1d20805b
schlotthauerjuan <juan_41629456gmail.com>
2024-11-14 20:07:20 -0300

    aiudame jebus

--- archivosJS/registro.js
+++ archivosJS/registro.js
@@ -3,14 +3,7 @@ const key_usuario = "key_usuario"
 let button = document.getElementById (`RegistroCompletado`)
 
 button.addEventListener("click",()=>{
-    let Nombre = document.getElementById(`Nombre`).value
-    let apellido = document.getElementById(`Apellido`).value
-    let contrasenia = document.getElementById(`Contrasenia`).value
-    let contrasenia2 = document.getElementById(`Contrasenia2`).value
-    let edad = document.getElementById(`Edad`).value
-    let telefono = document.getElementById(`Telefono`).value
-    let user = encontrar_usuario(Nombre, apellido, contrasenia, contrasenia2, edad, telefono)
-    let msj = Validar_usuario(Nombre, apellido,contrasenia,contrasenia2,edad,telefono)
+    
     
 
     if (user) {
@@ -22,10 +15,9 @@ button.addEventListener("click",()=>{
         let nuevo_usuario = new Usuario(Nombre, apellido, contrasenia, contrasenia2, edad, telefono)
         guardar_usuario(nuevo_usuario)
         alert("creado")
+    }
 
-    
-
-    let msj = new Array
+     
     if(msj.length == 0 ){
         let usuario = new Usuario(Nombre, apellido, contrasenia, edad, telefono)
 
@@ -34,9 +26,10 @@ button.addEventListener("click",()=>{
             
             
         }
-
+        
     }
 }
+)
 
 
 
@@ -92,7 +85,8 @@ function encontrar_usuario(usuarios) {
 
 
 function Validar_usuario() {
-    let msj = new Array
+
+    let msj = new Array()
     let Nombre = document.getElementById(`Nombre`).value
     let apellido = document.getElementById(`Apellido`).value
     let contrasenia = document.getElementById(`Contrasenia`).value
@@ -101,31 +95,37 @@ function Validar_usuario() {
     let telefono = document.getElementById(`Telefono`).value
     //let formularioIncompleto = getElementById("formularioIncompleto")
     
-
+    
     if (Nombre === "") {
         msj.push("Escribi tu nombre")
-        alert ("error nombre")
+        
     }
     if (apellido === "") {
         msj.push("Escribi tu apellido")
-        alert("error apellido")
+        
     }
     if (contrasenia === "" || contrasenia < 5) {
         msj.push("Debes poner una contraseña con al menos 5 dijitos ")
-        alert("error contra")
+        
     }
     if (contrasenia2 === "" || contrasenia2 != contrasenia) {
         msj.push("No confirmaste la contraseña o no son iguales")
-        alert("error contra")
+        
     }
     if (edad < 0 || !edad) {
         msj.push("Debes poner una edad")
-        alert("error edad")
+       
     }
     if (telefono === "") {
         msj.push("Debes colocar un telefono")
-        alert("error num")
+       
     }
-    return msj 
-
+    
+    if (msj.length == 0) {
+        return new Usuario
+    }else{
+        return msj
+    }
 }
+
+
