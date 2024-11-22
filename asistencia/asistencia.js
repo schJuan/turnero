54f506e7152a073b328159232afdb49b137403e5
schlotthauerjuan <juan_41629456gmail.com>
2024-11-22 14:54:21 -0300

    preentrega

--- asistencia/asistencia.js
+++ asistencia/asistencia.js
@@ -1,12 +1,15 @@
 let button_fui = document.getElementById("fui")
 let button_noFui = document.getElementById("noFui")
 
+button_noFui = addEventListener("click", () => {
 
+    let fechaDia = document.getElementById("fechaDia")
+    fechaDia.classList.add(`dia3`)
+    fechaDia.classList.remove(`dia2`)
+})
 button_fui = addEventListener("click",()=>{
     let fechaDia = document.getElementById("fechaDia")
-    fechaDia.classList.add (`dia2`)
+    fechaDia.classList.add(`dia2`)
+    fechaDia.classList.remove (`dia3`)
 })
-button_noFui = addEventListener("click",()=>{
-    let fechaDia = document.getElementById("fechaDia")
-    fechaDia.classList.add (`dia3`)
-})
+
